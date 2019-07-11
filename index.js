90a887e8f1482ca4b6224c37d9f4b14614544e4d
Tera Coder <TerableCoder@gmail.com>
2019-07-11 03:45:48 -0400

    added cycling

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
+String.prototype.clr = function (hexColor){ return `<font color='#${hexColor}'>${this}</font>` };
 module.exports = function TerableCrafter(mod) {
-  let petInfo = 0n;
+  let petInfo = 0n,
+    cycle = false,
+    timeout;
   mod.command.add(['terac', 'tcraft'], {
-    $default() {
+    $none() {
       if (!petInfo) return;
       mod.send('C_START_SERVANT_ACTIVE_SKILL', 1, {
         gameId: petInfo.gameId,
         id: 5024
       });
+    },
+    cycle(){
+      if(petInfo){
+        cycle = !cycle;
+        mod.command.message(`Cycling TCraft is now ${cycle ? "enabled".clr("00FF00") : "disabled".clr("FF0000")}.`);
+        clearInterval(timeout);
+        if(!cycle) return;
+        mod.send('C_START_SERVANT_ACTIVE_SKILL', 1, {
+          gameId: petInfo.gameId,
+          id: 5024
+        });
+    		timeout = setInterval(() => {
+          if(petInfo){
+            mod.send('C_START_SERVANT_ACTIVE_SKILL', 1, {
+              gameId: petInfo.gameId,
+              id: 5024
+            });
+          }
+    		}, 270000); // reapply every 4.5 mins
+      } else{
+        cycle = false;
+        mod.command.message(`No servant spawned. Cycling TCraft Disabled`.clr("FF0000"));
+      }
     }
   });
 
